194352d8624a45705648f55bc7ecb2cde1598dfc
Massive Brains <vadeshayo@gmail.com>
2016-04-16 12:53:29 +0100

    initialized all views to work with imported ui

--- application/views/includes/tables-footer.php
+++ application/views/includes/tables-footer.php
@@ -1,36 +1,36 @@
  
-<script src="assets/plugins/pace/pace.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery/jquery-1.11.1.min.js" type="text/javascript"></script>
-<script src="assets/plugins/modernizr.custom.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
-<script src="assets/plugins/boostrapv3/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery/jquery-easy.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-unveil/jquery.unveil.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-bez/jquery.bez.min.js"></script>
-<script src="assets/plugins/jquery-ios-list/jquery.ioslist.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-actual/jquery.actual.min.js"></script>
-<script src="assets/plugins/jquery-scrollbar/jquery.scrollbar.min.js"></script>
-<script type="text/javascript" src="assets/plugins/bootstrap-select2/select2.min.js"></script>
-<script type="text/javascript" src="assets/plugins/classie/classie.js"></script>
-<script src="assets/plugins/switchery/js/switchery.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-datatable/media/js/jquery.dataTables.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-datatable/extensions/TableTools/js/dataTables.tableTools.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-datatable/media/js/dataTables.bootstrap.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-datatable/extensions/Bootstrap/jquery-datatable-bootstrap.js" type="text/javascript"></script>
-<script type="text/javascript" src="assets/plugins/datatables-responsive/js/datatables.responsive.js"></script>
-<script src="assets/plugins/moment/moment.min.js"></script>
-<script src="assets/plugins/bootstrap-timepicker/bootstrap-timepicker.min.js"></script>
-<script type="text/javascript" src="assets/plugins/datatables-responsive/js/lodash.min.js"></script>
-<script src="assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js" type="text/javascript"></script>
-<script src="assets/plugins/bootstrap-daterangepicker/daterangepicker.js"></script>
-<script src="assets/plugins/toastr/toastr.min.js"></script>
-<script src="assets/js/tactic/play.js"></script>
-<script src="assets/js/tactic/game.js"></script>
-<script src="pages/js/pages.min.js"></script>
-<script src="assets/js/datatables.js" type="text/javascript"></script>
-<script src="assets/js/scripts.js" type="text/javascript"></script>
-<script src="assets/js/demo.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/pace/pace.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/jquery/jquery-1.11.1.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/modernizr.custom.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/boostrapv3/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/jquery/jquery-easy.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/jquery-unveil/jquery.unveil.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/jquery-bez/jquery.bez.min.js"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/jquery-ios-list/jquery.ioslist.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/jquery-actual/jquery.actual.min.js"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets/assets/plugins/bootstrap-select2/select2.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets/assets/plugins/classie/classie.js"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/switchery/js/switchery.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/jquery-datatable/media/js/jquery.dataTables.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/jquery-datatable/extensions/TableTools/js/dataTables.tableTools.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/jquery-datatable/media/js/dataTables.bootstrap.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/jquery-datatable/extensions/Bootstrap/jquery-datatable-bootstrap.js" type="text/javascript"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets/assets/plugins/datatables-responsive/js/datatables.responsive.js"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/moment/moment.min.js"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/bootstrap-timepicker/bootstrap-timepicker.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets/assets/plugins/datatables-responsive/js/lodash.min.js"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/toastr/toastr.min.js"></script>
+<script src="<?php echo base_url() ?>assets/assets/js/tactic/play.js"></script>
+<script src="<?php echo base_url() ?>assets/assets/js/tactic/game.js"></script>
+<script src="<?php echo base_url() ?>assets/pages/js/pages.min.js"></script>
+<script src="<?php echo base_url() ?>assets/assets/js/datatables.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/assets/js/scripts.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/assets/js/demo.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/assets/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
         <script>
       $(function(){
         $('#dataTable').dataTable();
